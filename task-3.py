9d1e2b84322ef2c8d7103e0b4e83226a0434ed57
Anton2002ff <anton.poloneichik232@gmail.com>
2024-11-17 22:19:50 +0300

    Извинте пожалуйста 6 урок еще не изучил,завален работой завтра все сделаю и остальные ДЗ

---
+++ task-3.py
@@ -0,0 +1,17 @@
+# Два упражнения по третьему занятию:
+#
+# Задание №1
+#
+# name = "John Show"
+# age = 29
+# print(name)
+# print(age)
+#
+# Задание №2
+#
+# my_age = 22
+# my_brother_age = my_age + 3
+# my_mom_age = my_age + 28
+# print(my_age)
+# print(my_brother_age)
+# print(my_mom_age)
---
+++ task_4.py
@@ -0,0 +1,30 @@
+# Два упражнения по четвертому занятию:
+
+ # Упражнение №1
+
+#само задание(надо исправить ошибки)
+#Health = 20
+#if (health < 0)
+#print(false)
+#else:
+#print(true)
+
+#Испраленно:
+# health = 20
+# if health > 0:
+#     print(False)
+# else:
+#     print(True)
+
+#Задание №2
+
+#Нужно исправить:
+# list = [122, True, "вишня"]
+# for x in range(1, list):
+# print x
+
+#Исправленно:
+
+# my_list = [122, True, "вишня"]
+# for x in my_list:
+#     print(x)
